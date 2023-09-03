a665e61fc225fff9ebc279a1676398d946a693ab
frankpyf <pingyifeng@yiban.cn>
2023-09-03 13:17:04 +0800

    change the back ground color to gray

--- src/engine_impl.cpp
+++ src/engine_impl.cpp
@@ -7,7 +7,7 @@ import window;
 import renderer;
 
 // 60 fps
-static constexpr float s_standard_delta_time = 1.0 / 60.0;
+static constexpr float s_ms_per_frame = 1.0 / 60.0;
 
 Engine::Engine()
 {
@@ -23,11 +23,11 @@ void Engine::run()
 
     tetris_.init_debug();
 
-    float dt = s_standard_delta_time;
+    float dt = s_ms_per_frame;
     uint64_t begin_time = SDL_GetTicks64();
     while(!should_close_)
     {
-        renderer.render_clear_color(0, 255, 0, 1);
+        renderer.render_clear_color(122, 122, 122, 1);
         
         // Handle Input
         while(SDL_PollEvent(&event)) {
@@ -51,8 +51,8 @@ void Engine::run()
         dt = (end_time - begin_time) / 1000.0f;
         begin_time = end_time;
 
-        if(dt < s_standard_delta_time)
-            SDL_Delay(s_standard_delta_time - dt);
+        if(dt < s_ms_per_frame)
+            SDL_Delay(s_ms_per_frame - dt);
     }
     std::cout<<"Engine running"<<std::endl;
 }
